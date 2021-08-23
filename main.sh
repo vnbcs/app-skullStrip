ad6e3d2b8e6222234dfcd6f4395fdc638f37cc88
eva bacas <ENB30@pitt.edu>
2021-08-23 13:07:36 -0400

    update and rename main

--- main.sh
+++
@@ -1,5 +0,0 @@
-# this app runs afni's skullstrip function over a nifti file
-
-# i'm actually not sure if brainlife's docker image for afni contains jq bc i have been testing this locally
-# but if it doesn't i will ask them to add it
-time singularity exec -e docker://brainlife/afni:16.3.0 ./afni_skullstrip.sh
