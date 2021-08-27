a864b6d8f1139dccb18c0f4164a20546af065f4d
Eva Bacas <ENB30@pitt.edu>
2021-08-27 10:24:20 -0400

    using my own docker file

--- afni_skullstrip.sh
+++ afni_skullstrip.sh
@@ -1,6 +1,13 @@
 # trying to make AFNI not whine about lack of display
 #DISPLAY=localhost:0.0
 
+Xvfb :88 &
+export DISPLAY=:88
+#Xvfb :88 -screen 0 1024x768x24 >& /dev/null &
+#setenv DISPLAY :88
+#unsetenv DISPLAY
+#killall Xvfb
+
 # load filename and optional parameters
 t1=`jq -r '.t1' config.json`
 optional_params=`jq -r '.optional_params' config.json`
@@ -16,18 +23,9 @@ mkdir -p skullStrip
 # move file to new directory skullStrip
 install $t1 skullStrip/t1.nii.gz
 
-# if [ -f ./skullStrip/t1.nii.gz ];
-# then
-#   echo "file exists. skipping copying"
-# else
-#   cp -v ${t1} ./skullStrip/t1.nii.gz;
-# fi
-
 # run 3dSkullStrip
 if [ "$optional_params" != "null" ]; then
-    # 3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix anat_ss ${optional_params}
     3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn} ${optional_params}
 else
-    # 3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix anat_ss
     3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn}
 fi
